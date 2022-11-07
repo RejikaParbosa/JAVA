e120a3ddcfc16e160c6cadfa2efcd0272a74af41
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2022-11-07 13:12:30 +0530

    Alphabetical Order of String

---
+++ StringOverloading.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class StringOverloading
+{
+	public static void main(String args[])
+	{
+		String name[]={"Delhi","Assam","Mumbai","Punjab"};
+		int size = name.length;
+		String temp=null;
+		
+		for(int i=0;i<size;i++)
+		{
+			for(int j=i+1;j<size;j++)
+			{
+				if(name[j].compareTo(name[i])<0)
+				{
+					temp=name[i];
+					name[i]=name[j];
+					name[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<size;i++)
+			System.out.println(name[i]);
+	}
+}	
